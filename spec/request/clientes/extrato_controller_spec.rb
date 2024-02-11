a38077ba21aac5277afa9043403b759b1d0472d4
PedroAugustoRamalhoDuarte <pedroaugustorduarte@gmail.com>
2024-02-11 11:16:02 -0300

    testing 404 error for client

--- spec/request/clientes/extrato_controller_spec.rb
+++ spec/request/clientes/extrato_controller_spec.rb
@@ -13,5 +13,14 @@ RSpec.describe "Cliente::ExtratoController", type: :request do
       expect(JSON.parse(response.body)["saldo"]["total"]).to eq(100)
       expect(JSON.parse(response.body)["ultimas_transacoes"][0]["valor"]).to eq(100)
     end
+
+    context "quando o cliente não existe" do
+      it 'retorna 404' do
+        get "/clientes/0/extrato"
+
+        expect(response).to have_http_status(:not_found)
+        expect(JSON.parse(response.body)["message"]).to eq("Cliente não encontrado")
+      end
+    end
   end
 end
