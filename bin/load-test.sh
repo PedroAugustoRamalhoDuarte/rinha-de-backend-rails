7b92e35e9a83e4662dc02979abe91b99de95bca5
PedroAugustoRamalhoDuarte <pedroaugustorduarte@gmail.com>
2024-02-11 11:16:10 -0300

    load test sh

---
+++ bin/load-test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+docker-compose stop
+
+docker-compose start db
+
+RAILS_ENV=production RINHA_DE_BACKEND_DATABASE_PASSWORD=postgres DISABLE_DATABASE_ENVIRONMENT_CHECK=1 rails db:reset
+
+docker-compose up -d --build
