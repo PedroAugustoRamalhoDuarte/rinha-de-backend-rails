0ad83e61ed5cae625dd4317bb06c62f80c0c68a2
PedroAugustoRamalhoDuarte <pedroaugustorduarte@gmail.com>
2024-02-14 07:05:58 -0300

    Adição do campo saldo na tabela de usuários

--- app/controllers/clientes/transacoes_controller.rb
+++ app/controllers/clientes/transacoes_controller.rb
@@ -1,11 +1,13 @@
 class Clientes::TransacoesController < ApplicationController
   def create
     begin
-      cliente = Cliente.find(params[:id])
-      cliente.transacoes.create!(transacao_params)
+      Cliente.transaction do
+        @cliente = Cliente.find(params[:id])
+        @cliente.transacoes.create!(transacao_params)
+      end
       render json: {
-        limite: cliente.limite,
-        saldo: cliente.saldo,
+        limite: @cliente.limite,
+        saldo: @cliente.saldo,
       }, status: :ok
     rescue ActiveRecord::RecordInvalid => e
       render json: { errors: e.record.errors }, status: :unprocessable_entity
